cf0c3209a355e3c0cfcb9a319392e279dd6a8b28
Mike Warner <minimike86@gmail.com>
2022-02-13 17:19:39 +0000

    update runner names to scroll if oversized

--- src/app/components/obs/standard-side-panel/ssp-runner-name/ssp-runner-name.component.ts
+++ src/app/components/obs/standard-side-panel/ssp-runner-name/ssp-runner-name.component.ts
@@ -3,7 +3,7 @@ import { faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { GameLineupService } from '../../../../services/firebase/game-lineup/game-lineup.service';
 import { ScheduledVideoGame } from '../../../../models/video-game';
 import { CurrentlyPlayingService } from '../../../../services/firebase/currently-playing/currently-playing.service';
-import { map } from 'rxjs/operators';
+import {BehaviorSubject, Observable, of, switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-ssp-runner-name',
@@ -11,27 +11,47 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./ssp-runner-name.component.css']
 })
 export class SspRunnerNameComponent implements OnInit {
+  public currentGame$: Observable<string>;
   public currentGame: string;
-  public zeldaGame: ScheduledVideoGame = new ScheduledVideoGame(null, null, null, null, null, 0);
+
+  public gameLineUp$: Observable<ScheduledVideoGame[]>;
   public gameLineUp: ScheduledVideoGame[];
+
+  public zeldaGame$: BehaviorSubject<ScheduledVideoGame> = new BehaviorSubject<ScheduledVideoGame>(null);
+
   public faTwitch = faTwitch;
   public faYoutube = faYoutube;
 
   constructor( private gameLineupService: GameLineupService,
                private currentlyPlayingService: CurrentlyPlayingService ) {
-
-    this.currentlyPlayingService.getCurrentlyPlaying().pipe(map(data => {
-      this.currentGame = data[0].index;
-    })).subscribe();
-
-    this.gameLineupService.getGameLineUp().pipe(map(data => {
-      this.gameLineUp = data.find(x => x.id === 'ACTIVE-SCHEDULE').activeSchedule;
-      this.zeldaGame = this.gameLineUp.find(x => x.gameProgressKey === this.currentGame);
-    })).subscribe();
-
   }
 
   ngOnInit() {
+    this.currentGame$ = this.currentlyPlayingService.getCurrentlyPlaying().pipe(switchMap(data => {
+      this.currentGame = data[0].index;
+      this.updateZeldaGame();
+      console.log('currentGame update:', this.currentGame);
+      return of(this.currentGame);
+    }));
+
+    this.gameLineUp$ = this.gameLineupService.getGameLineUp().pipe(switchMap(data => {
+      this.gameLineUp = data.find(x => x.id === 'ACTIVE-SCHEDULE').activeSchedule;
+      this.updateZeldaGame();
+      console.log('gameLineUp update:', this.gameLineUp);
+      return of(this.gameLineUp);
+    }));
+  }
+
+  updateZeldaGame() {
+    this.zeldaGame$.next(this.gameLineUp?.find(x => x.gameProgressKey === this.currentGame));
+  }
+
+  getRunnersCharLength(game: ScheduledVideoGame): number {
+    let charLen = 0;
+    for (const runner of game.runners) {
+      charLen += runner.name.length;
+    }
+    return charLen;
   }
 
 }
